b6ea1540687bfbf1c7b8d7f7630ac5f0909914e2
daria <daaria.suv@yandex.ru>
2023-10-22 21:53:02 +0300

    checkstyle error again

--- src/main/java/edu/hw2/task4/CallingInfoMethod.java
+++ src/main/java/edu/hw2/task4/CallingInfoMethod.java
@@ -2,6 +2,9 @@ package edu.hw2.task4;
 
 public class CallingInfoMethod {
 
+
+    private CallingInfoMethod() {}
+
     public static CallingInfo callingInfo(Throwable e) {
         StackTraceElement[] trace = e.getStackTrace();
         return new CallingInfo(trace[0].getClassName(), trace[0].getMethodName());
